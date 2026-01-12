e13285fb85d04b0945337b724cab7809af1eec81
qingyun-tuberlin <qingyuntub0@gmail.com>
2026-01-12 20:10:12 +0100

    add file to check coding environment

--- Task3ComparisonNeuralLM.py
+++ Task3ComparisonNeuralLM.py
@@ -1,21 +1,6 @@
 
-import re
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from collections import Counter
-from wordcloud import WordCloud
 import torch
-from tqdm.auto import tqdm
-from sklearn.feature_extraction.text import TfidfVectorizer
-import nltk
-from nltk.util import ngrams
-from nltk.tokenize import word_tokenize,RegexpTokenizer
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer,WordNetLemmatizer
-from transformers import AutoTokenizer
-
-
 """
     Task 3.2 – Comparison with a Neural Language Model
     Build a simple neural language model consisting of:
